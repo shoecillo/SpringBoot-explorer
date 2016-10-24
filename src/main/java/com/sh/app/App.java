f0e85be35fd80528f5cb370992b4d5f0932ddd88
PABLO VALLEJO MUÑOZ <0009749@insags.com>
2016-10-24 16:27:52 +0200

    initial upload

---
+++ src/main/java/com/sh/app/App.java
@@ -0,0 +1,17 @@
+package com.sh.app;
+
+import org.springframework.boot.SpringApplication;
+
+import com.sh.config.ConfigApp;
+
+
+public class App 
+{
+	
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 SpringApplication.run(ConfigApp.class, args);
+	 }
+	
+}
---
+++ src/main/java/com/sh/config/ConfigApp.java
@@ -0,0 +1,14 @@
+package com.sh.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sh.ctrl")
+@EnableAutoConfiguration
+public class ConfigApp {
+	
+	
+	
+}
